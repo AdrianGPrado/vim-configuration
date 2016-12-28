a07e020a4e5f1f571d4d9a548c52bff46ff45614
AG Prado <agprado@nrg.com>
2016-12-27 19:44:23 -0800

    Split list of managers and colors

--- configure_vim.py
+++ configure_vim.py
@@ -3,13 +3,18 @@
 import os
 import subprocess
 
+PLUGIN_MANAGER = ["https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"]
+
+COLORS = ["https://raw.githubusercontent.com/fmoralesc/molokayo/master/colors/molokayo.vim"]
+
 
 def downloadSourceURL(source, dest):
     """
         Use curl to download a file defined with a URL, and download
     """
     # Create the folder path. If it exists don't raise exeption
-    os.makedirs(dest, exist_ok=True)
+    if not os.path.exists(source):
+        os.mkdir(source)
     # extract file name from source url
     name = source.split("/")[-1]
     # Download the file
@@ -19,27 +24,28 @@ def downloadSourceURL(source, dest):
 def downloadConfigFiles(base_dir):
 
     # Define vim directory
-    vimAutoload     = base_dir + "vim/autoload"
-    vimColors       = base_dir + "vim/colors"
-    vimPluggins     = base_dir + "vim/pluggins"
+    vimAutoload     = base_dir + "/vim/autoload"
+    vimColors       = base_dir + "/vim/colors"
+    vimPluggins     = base_dir + "/vim/pluggins"
 
     """ vim-plugin management file
     VIM_PLUGIN_FILE: (str)
     PLUGIN_DEST:     (str) where to locate 'plug.vim' file
     """
-    files = [("https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"            , vimAutoload ),
-             ("https://raw.githubusercontent.com/fmoralesc/molokayo/master/colors/molokayo.vim", vimColors   ) ]
 
-    for source, dest in files:
-        downloadSourceURL(source, dest)
+    for source in PLUGIN_MANAGER:
+        downloadSourceURL(source, vimAutoload)
+
+    for source in COLORS:
+        downloadSourceURL(source, vimColors)
 
 def linkConfigDirectories(baseDir, homeDir):
 
-    linkedConf = [(baseDir + "/vimrc", homeDir + "/.vimrc"),
-                  (baseDir + "/vim"  , homeDir + "/.vim"  ) ]
+    linkedConf = ["vimrc", "vim"]
 
-    for source, dest in linkedConf:
-        subprocess.run(args=["ln", "-sf", source, dest])
+    for fname in linkedConf:
+        subprocess.run(args=["ln", "-sf", homeDir + "/"  + fname,
+                                          baseDir + "/." + fname ])
 
 def main():
 
